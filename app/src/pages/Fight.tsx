f6c5ca5453748ba8e8f4b2bca2403950e940fb6a
Liv44 <moreauolivia49@gmail.com>
2022-05-30 23:40:35 +0200

    feat(app/ package.json, App.tsx index.tsx Fight.tsx Home.tsx): Create Architecture, add axios and react-router-dom, add routes home and fight

---
+++ app/src/pages/Fight.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from "react";
+// interface FightProps {}
+export const Fight: FC = () => {
+  return <h1>Fight</h1>;
+};
---
+++ app/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+// interface HomeProps {}
+export const Home: FC = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+};
