d78adabe7ef0f5c561695823edd34cbcc2c17de2
Liv44 <moreauolivia49@gmail.com>
2022-05-31 16:02:13 +0200

    feat(App.tsx, Home.tsx):header created

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,3 +1,4 @@
+import { Box, Button, Heading, HStack, Text } from "@chakra-ui/react";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -5,11 +6,25 @@ import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-      </Routes>
-    </Router>
+    <Box backgroundColor="background" minHeight="100vh">
+      <HStack
+        padding="3"
+        color="white"
+        backgroundColor="blue"
+        justifyContent="space-between"
+      >
+        <Heading>RPG theTribe</Heading>
+        <HStack>
+          <Text>Connecté en tant que : "nom de l'utilisateur"</Text>
+          <Button backgroundColor="yellow"> Déconnexion</Button>
+        </HStack>
+      </HStack>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </Router>
+    </Box>
   );
 }
 
--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -2,11 +2,5 @@ import { Heading } from "@chakra-ui/react";
 import React, { FC } from "react";
 // interface HomeProps {}
 export const Home: FC = () => {
-  return (
-    <div>
-      <Heading backgroundColor="blue" color="white">
-        Home
-      </Heading>
-    </div>
-  );
+  return <div></div>;
 };
